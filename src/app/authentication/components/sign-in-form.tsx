35f71903117f54f469542bb4fb79ba0cea6482c1
BrunoRodri <fidbruno@hotmail.com>
2025-08-06 17:08:00 -0300

    feat: add sign in with email and password

--- src/app/authentication/components/sign-in-form.tsx
+++ src/app/authentication/components/sign-in-form.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -21,6 +23,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
   email: z.email("Email Inválido!"),
@@ -30,6 +33,7 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>;
 
 const SignInForm = () => {
+  const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,9 +42,34 @@ const SignInForm = () => {
     },
   });
 
-  function onSubmit(values: FormValues) {
-    console.log("FORMULARIO VALIDO E ENVIADO!");
-    console.log(values);
+  async function onSubmit(values: FormValues) {
+    await authClient.signIn.email({
+      email: values.email,
+      password: values.password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code === "USER_NOT_FOUND") {
+            toast.error("Usuário não encontrado!");
+            return form.setError("email", {
+              message: "Usuário não encontrado!",
+            });
+          }
+          if (ctx.error.code === "INVALID_EMAIL_OR_PASSWORD") {
+            toast.error("Email ou senha inválidos!");
+            form.setError("password", {
+              message: "Email ou senha inválidos!",
+            });
+            return form.setError("email", {
+              message: "Email ou senha inválidos!",
+            });
+            
+          }
+        },
+      },
+    });
   }
 
   return (
@@ -48,9 +77,7 @@ const SignInForm = () => {
       <Card>
         <CardHeader>
           <CardTitle>Entrar</CardTitle>
-          <CardDescription>
-            Faça login para continuar.
-          </CardDescription>
+          <CardDescription>Faça login para continuar.</CardDescription>
         </CardHeader>
 
         <Form {...form}>
